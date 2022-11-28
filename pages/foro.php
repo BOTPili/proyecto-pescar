4d9f131487d187b8b8a3c5525f24760aa3d29db9
Uriel Milgron <urielmilgron18@gmail.com>
2022-11-27 21:22:00 -0300

    css foro added

--- pages/foro.php
+++ pages/foro.php
@@ -10,13 +10,15 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="../css/estilos.css">
+    <link rel="stylesheet" href="../css/foro.css">
     <link rel="shortcut icon" href="../images/logo-educarte.png" type="image/x-icon">
     <title>Foro</title>
 </head>
 
 <body>
     <?php include_once 'navbar.php' ?>
-    <main class="foro-main">
+    <div class="foro-main-container">
+        <div class="foro-main">
         <div class="foro-container-title">
             <h2 class="foro-title">FORO</h2>
         </div>
@@ -140,7 +142,8 @@
                 </article>
             </section>
         </section>
-    </main>
+        </div>
+</div>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/js/bootstrap.bundle.min.js"
         crossorigin="anonymous"></script>
 </body>
