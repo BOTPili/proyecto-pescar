b029f301ead8fcf53d7472b29f51b939b4c5dad5
Dante Roldan <danteroldan72@gmail.com>
2022-11-30 22:52:29 -0300

    cambios

--- prueba.js
+++ prueba.js
@@ -1,17 +1,209 @@
 
-let url = "http://localhost:3000/infantiles";
-let urlNumeros = "http://localhost:3000/numeros";
-let urlColores = "http://localhost:3000/colores";
+  let colores = [
+       {
+           "color": "Rojo"
+   }, 
+       {
+           "color": "Azul"
+   }, 
+       {
+           "color": "Amarillo"
+   }, 
+       {
+           "color": "Verde"
+   }, 
+       {
+           "color": "Naranja"
+   }, 
+       {
+           "color": "Violeta"
+   }, 
+       {
+           "color": "Blanco"
+   }, 
+       {
+           "color": "Negro"
+   }
+]
+   
 
-fetch(url)
-.then(response => response.json())
-.then(dato => mostrarDatos(dato))
-.catch(error => console.log(error))
+
+   let numeros = [
+       {
+           "numero": 1
+       },
+       {
+           "numero": 2
+       },
+       {
+           "numero": 3
+       },
+       {
+           "numero": 4
+       },
+       {
+           "numero": 5
+       },
+       {
+           "numero": 6
+       },
+       {
+           "numero": 7
+       },
+       {
+           "numero": 8
+       },
+       {
+           "numero": 9
+       },
+       {
+           "numero": 0
+       }
+   ]
+
+   
+   
+    let infantiles = [
+       {
+           "letra": "A",
+           "en": "Ingles",
+           "es": "Español"
+       },
+
+       {
+           "letra": "B",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "C",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "D",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "E",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "F",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "G",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "H",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "I",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "J",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "K",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "L",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "M",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "N",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "Ñ",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "O",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "P",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "Q",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "R",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "S",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "T",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "U",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "W",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "X",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "Y",
+           "en": "Ingles",
+           "es": "Español"
+       },
+       {
+           "letra": "Z",
+           "en": "Ingles",
+           "es": "Español"
+       }
+   ]
+
+
+
+
 
 const carta = document.querySelector('.carta');
 
-const mostrarDatos = (dato) =>{
-   dato.forEach(function(i){
+   infantiles.forEach(function(i){
       carta.innerHTML += `<div class="card my-2 mx-2" style="width: 12rem; height: 180px;">
       <h1 class="text-center mt-2">${i.letra} </h1>
      <div class="card-footer mt-5 d-flex justify-content-center">
@@ -22,18 +214,15 @@ const mostrarDatos = (dato) =>{
 
    });
 
-}
+
 
 
 const color = document.querySelector('.color');
 
-fetch(urlColores)
-.then(response => response.json())
-.then(dato => mostrarColor(dato))
-.catch(error => console.log(error))
 
-const mostrarColor = (dato) => {
-   dato.forEach(function(c){
+
+
+   colores.forEach(function(c){
       color.innerHTML += `<div class="card my-2 mx-2" style="width: 12rem; height: 180px;">
       <h1 class="text-center mt-2">${c.color} </h1>
        <div class="card-footer mt-5 d-flex justify-content-center">
@@ -44,16 +233,12 @@ const mostrarColor = (dato) => {
   
      });
   
-}
+
 
 const numero = document.querySelector('.numero');
-fetch(urlNumeros)
-.then(response => response.json())
-.then(dato => mostrarNumeros(dato))
-.catch(error => console.log(error))
 
-const mostrarNumeros = (dato) => {
-   dato.forEach(function(n){
+
+   numeros.forEach(function(n){
       numero.innerHTML += `<div class="card my-2 mx-2" style="width: 12rem; height: 180px;">
       <h1 class="text-center mt-2">${n.numero} </h1>
        <div class="card-footer mt-5 d-flex justify-content-center">
@@ -64,7 +249,7 @@ const mostrarNumeros = (dato) => {
   
      });
   
-}
+
 
 
 // dato.forEach(function(i){
